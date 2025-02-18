ef6beb55a5baf0e1dc81747332b76393bec1b18c
Saad Ahmed <saadahmedez1@gmail.com>
2025-02-19 01:01:09 +0200

    Add Blog Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Services from "./components/Services";
+import Blog from "./components/Blog";
 function App() {
   return (
     <>
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <About />
       <Services/>
+      <Blog/>
     </>
   );
 }
---
+++ src/components/Blog.jsx
@@ -0,0 +1,62 @@
+function Blog() {
+  return (
+    <>
+      <section className="bg-[#dee2e6] py-16" id="blog">
+        <div class="container m-auto px-4">
+          <h2 className="text-center text-3xl font-bold">Blog</h2>
+          <div className="flex justify-center flex-wrap gap-10 mt-5">
+            <div className="flex items-center flex-wrap justify-center gap-4 rounded-lg bg-[#dbd6d6]">
+            <img className="rounded-lg w-full md:w-auto" src="images/javascript.webp" alt="javascript" />
+              <div class="description">
+                <h3 className="mb-5 text-3xl font-bold">Learn Python</h3>
+                <p>
+                  Lorem ipsum dolor <br />
+                  sit amet, consecte <br />
+                  adipiscing elit. Proin <br />
+                  tristique elit nec. lorem ipsum
+                </p>
+              </div>
+            </div>
+            <div className="flex items-center flex-wrap justify-center gap-4 rounded-lg bg-[#dbd6d6]">
+              <img className="rounded-lg w-full md:w-auto" src="images/javascript.webp" alt="javascript" />
+              <div class="description">
+                <h3 className="mb-5 text-3xl font-bold">Learn Javascript</h3>
+                <p>
+                  Lorem ipsum dolor <br />
+                  sit amet, consecte <br />
+                  adipiscing elit. Proin <br />
+                  tristique elit nec. lorem ipsum
+                </p>
+              </div>
+            </div>
+            <div className="flex items-center flex-wrap justify-center gap-4 rounded-lg bg-[#dbd6d6]">
+            <img className="rounded-lg w-full md:w-auto" src="images/javascript.webp" alt="javascript" />
+              <div class="description">
+                <h3 className="mb-5 text-3xl font-bold">Learn UiUx</h3>
+                <p>
+                  Lorem ipsum dolor <br />
+                  sit amet, consecte <br />
+                  adipiscing elit. Proin <br />
+                  tristique elit nec. lorem ipsum
+                </p>
+              </div>
+            </div>
+            <div className="flex items-center flex-wrap justify-center gap-4 rounded-lg bg-[#dbd6d6]">
+            <img className="rounded-lg w-full md:w-auto" src="images/javascript.webp" alt="javascript" />
+              <div class="description">
+                <h3 className="mb-5 text-3xl font-bold">Learn Rust</h3>
+                <p>
+                  Lorem ipsum dolor <br />
+                  sit amet, consecte <br />
+                  adipiscing elit. Proin <br />
+                  tristique elit nec. lorem ipsum
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+export default Blog;
